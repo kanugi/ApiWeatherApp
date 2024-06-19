da63322e607a515b2547fd00d188c808a27b0f5a
Kanikayudha Tantyo N <kanikayudha.17@students.amikom.ac.id>
2024-06-19 13:12:36 +0700

    add search function in app

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { BASE_URL, API_KEY } from './src/constant'
 import { View, StyleSheet } from 'react-native'
 import WeatherSearch from './src/components/weatherSearch'
 import WeatherInfo from './src/components/weatherInfo'
 
 const App = () => {
+  const [weatherData, setWeatherData] = useState()
+
+  const searchWeather = (location) => {
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data
+        // Tambahkan code di bawah
+        data.visibility /= 1000
+        data.visibility = data.visibility.toFixed(2)
+        data.main.temp -= 273.15 // Konversi Kelvin ke Celcius
+        data.main.temp = Math.round(data.main.temp);
+        setWeatherData(data)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
   return (
     <View style={styles.container}>
-      <WeatherSearch />
-      <WeatherInfo />
+      <WeatherSearch searchWeather={searchWeather} />
+      {/* Tampilkan data cuaca ketika ada weatherData */}
+      {weatherData && <WeatherInfo weatherData={weatherData} />}
     </View>
   )
 }
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = '3d28eb0c83f823c6ba986d461bc81761';
