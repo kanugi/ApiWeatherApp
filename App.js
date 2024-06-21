cda9ae478e295dff5c20cfa8a42d1e45017d1df8
Kanikayudha Tantyo N <kanikayudha.17@students.amikom.ac.id>
2024-06-21 10:13:37 +0700

    add feature status for the app

--- App.js
+++ App.js
@@ -1,35 +1,61 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { BASE_URL, API_KEY } from './src/constant'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, ActivityIndicator } from 'react-native'
 import WeatherSearch from './src/components/weatherSearch'
 import WeatherInfo from './src/components/weatherInfo'
 
 const App = () => {
   const [weatherData, setWeatherData] = useState()
 
+  // definisikan state status 
+  const [status, setStatus] = useState('')
+
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size="large" />
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />
+      case 'error':
+        return (
+          <Text style={styles.errorStatus}>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        )
+      default:
+        return
+    }
+  }
+  
   const searchWeather = (location) => {
+    // Mengatur status ke "loading"
+    setStatus('loading')
     axios
       .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
       .then((response) => {
         const data = response.data
-        // Tambahkan code di bawah
         data.visibility /= 1000
         data.visibility = data.visibility.toFixed(2)
-        data.main.temp -= 273.15 // Konversi Kelvin ke Celcius
+        data.main.temp -= 273.15
         data.main.temp = Math.round(data.main.temp);
         setWeatherData(data)
+        // Mengatur status ke "success"
+        setStatus('success')
       })
       .catch((error) => {
-        console.log(error);
+        // Mengatur status ke "error"
+        setStatus('error')
       })
   }
 
   return (
     <View style={styles.container}>
       <WeatherSearch searchWeather={searchWeather} />
-      {/* Tampilkan data cuaca ketika ada weatherData */}
       {weatherData && <WeatherInfo weatherData={weatherData} />}
+      <View style={styles.marginTop20}>
+        {renderComponent()}
+      </View>
     </View>
   )
 }
@@ -37,6 +63,10 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     padding: 50,
+  },
+  errorStatus: {
+    paddingHorizontal: 20,
+    color: "#fff",
   },
 })
 
